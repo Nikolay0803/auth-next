b2e4c097c80b402c199d798b78bc52b2c53c3734
Mykola_Maslakai <96542705+Nikolay0803@users.noreply.github.com>
2024-08-15 18:32:41 +0300

    styles: add new styles

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 const DashboardPage = () => {
-  return <div>Dashboard</div>;
+  return <div className="flex justify-center text-2xl pt-10">Dashboard</div>;
 };
 
 export default DashboardPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center items-center text-2xl pt-10">
       <p>Hi, You Are Watching Dashboard</p>
     </div>
   );
