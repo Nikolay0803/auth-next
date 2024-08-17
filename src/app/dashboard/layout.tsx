e3b897a52d82892efd286b4b3a9f4e030d566123
Mykola_Maslakai <96542705+Nikolay0803@users.noreply.github.com>
2024-08-17 17:20:53 +0300

    update layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,23 +1,10 @@
-import { authOptions } from "@/lib/authOptions";
-import { getServerSession } from "next-auth";
-import Link from "next/link";
-
 type Props = {
   children: React.ReactNode;
 };
 
-const DashBoardLayout = async (props: Props) => {
-  // const session = await getServerSession(authOptions);
+const DashBoardLayout = (props: Props) => {
   return (
     <>
-      {/* <div className="grid-cols-4 border-r shadow h-screen p-2">
-        <Link
-          className="p-3 rounded hover:bg-emerald-600 hover:text-white hover:shadow transition "
-          href={`/dashboard/user/${session?.user.id}`}
-        >
-          User Profile
-        </Link>
-      </div> */}
       <div>{props.children}</div>
     </>
   );
